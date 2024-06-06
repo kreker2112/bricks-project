722974378b4d8d97a880d34ab5628a55715a0177
kreker2112 <kreker2112@gmail.com>
2024-06-06 18:39:58 +0300

    update public

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "@vue/cli-service";
-import { DefinePlugin } from "webpack";
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
 
-export default defineConfig({
+module.exports = defineConfig({
   publicPath: "/Mozaic/",
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
-      new DefinePlugin({
+      new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: JSON.stringify(true),
         __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),

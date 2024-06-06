77b9771ef8ae9459af349840644a8ccd7a5f1205
kreker2112 <kreker2112@gmail.com>
2024-06-07 00:36:23 +0300

    update commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/global.css";
-// import "./assets/css/tailwind.css";
 import store from "./store";
 
 createApp(App).use(router).use(store).mount("#app");
